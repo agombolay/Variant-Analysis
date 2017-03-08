64f54b0b84bf4d067858a3212f3e812e8bbc1562
Alli Gombolay <agombolay3@gmail.com>
2017-03-08 16:43:32 -0500

    Update call-variants.sh

--- call-variants.sh
+++ call-variants.sh
@@ -54,11 +54,13 @@ fi
 #bowtie2 -x sacCer2 -1 $sample-R1Paired.fq -2 $sample-R2Paired.fq \
 #-U $sample-R1Unpaired.fq,$sample-R2Unpaired.fq -S $sample.sam
 
+samtools view -b -S $sample.sam | samtools sort -o $sample-sorted.bam -
+
 #SAM to BAM
-samtools view -b -S $sample.sam > $sample.bam
+#samtools view -b -S $sample.sam > $sample.bam
 
 #Sort BAM file
-samtools sort -o $sample-sorted.bam $sample.bam
+#samtools sort -o $sample-sorted.bam $sample.bam
 
 #Create index file
 samtools index $sample-sorted.bam
