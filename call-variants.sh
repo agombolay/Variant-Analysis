00e3dc5e01b94ee9781c29adba8101328dcc2ace
Alli Gombolay <agombolay3@gmail.com>
2017-03-07 20:18:46 -0500

    Update call-variants.sh

--- call-variants.sh
+++ call-variants.sh
@@ -1,3 +1,37 @@
-java -jar trimmomatic-0.36.jar PE -phred33 input_forward.fq.gz input_reverse.fq.gz \
-output_forward_paired.fq.gz output_forward_unpaired.fq.gz output_reverse_paired.fq.gz output_reverse_unpaired.fq.gz \
-ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:75
+#!/usr/bin/env bash
+
+#Author: Alli Gombolay
+#Date: March 7, 2017
+#This script uses Trimmomatic program to remove Illumina adapter sequences and trim reads based on quality
+#The goal of this pre-processing step is to increase mapping percentage and also decrease incorrect mappings
+
+#Usage statement of the program
+function usage () {
+        echo "Usage: call-variants.sh [-i] 'FASTQ' [-t] '/path/to/Trimmomatic/' [-h]
+              -f Path to input forward FASTQ.GZ files ('/path/to/forward.fastq.gz')
+              -r Path to input reverse FASTQ.GZ files ('/path/to/reverse.fastq.gz')
+		          -t '/projects/home/agombolay3/data/bin/Trimmomatic-0.36'"
+}
+
+#Command-line options
+while getopts "f:r:t:h" opt;
+do
+    case $opt in
+        f ) inputForward=$OPTARG ;;
+        r ) inputReverse=$OPTARG ;;
+        t ) path=$OPTARG ;;
+        #Print usage statement
+        h ) usage ;;
+    esac
+done
+
+#Exit program if user specifies [-h]
+if [ "$1" == "-h" ];
+then
+        exit
+fi
+
+#Trim FASTQ files based on quality and Illumina adapter content (adapter read-through)
+java -jar $path/trimmomatic-0.36.jar PE -phred33 $inputForward $inputReverse $sample-ForwardPaired.fastq.gz \
+$sample-ForwardUnpaired.fastq.gz $sample-ReversePaired.fastq.gz $sample-ReverseUnpaired.fastq.gz \
+ILLUMINACLIP:$path/adapters/TruSeq3-SE.fa:2:30:10 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:75
