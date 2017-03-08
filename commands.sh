d855d860f83d264f714fe81e762a59b434a2412a
Alli Gombolay <agombolay3@gmail.com>
2017-03-08 12:53:19 -0500

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,2 @@
+./call-variants.sh -a '/projects/home/agombolay3/data/repository/Variant-Analysis-Project/Sequencing-Results/YS486-1_S49_L001_R1_001.fastq.gz' -b '/projects/home/agombolay3/data/repository/Variant-Analysis-Project/Sequencing-Results/YS486-1_S49_L002_R1_001.fastq.gz' \
+-c '/projects/home/agombolay3/data/repository/Variant-Analysis-Project/Sequencing-Results/YS486-1_S49_L001_R2_001.fastq.gz' -d '/projects/home/agombolay3/data/repository/Variant-Analysis-Project/Sequencing-Results/YS486-1_S49_L002_R2_001.fastq.gz' -s 'YS486-1'
