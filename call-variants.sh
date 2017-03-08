b5622729b6c4964032e24050532090d08e7489bd
Alli Gombolay <agombolay3@gmail.com>
2017-03-08 15:04:01 -0500

    Update call-variants.sh

--- call-variants.sh
+++ call-variants.sh
@@ -52,7 +52,7 @@ fi
 #STEP 2
 #Align trimmed read pairs to reference genome of interest
 bowtie2 -x sacCer2 -1 $sample-R1Paired.fq -2 $sample-R2Paired.fq \
--U $sample-R1Unpaired.fq $sample-R2Unpaired.fq -S $sample.sam
+-U $sample-R1Unpaired.fq,$sample-R2Unpaired.fq -S $sample.sam
 
 #SAM to BAM
 #samtools view -b -S $sample.sam > $sample.bam
