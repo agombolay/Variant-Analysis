f73ef867b3287ad46e55adfe757f3aaef2b2786f
Alli Gombolay <agombolay3@gmail.com>
2017-04-07 13:06:09 -0400

    Create deNovo.sh

---
+++ deNovo.sh
@@ -0,0 +1,2 @@
+/projects/home/agombolay3/data/bin/SPAdes-3.10.1-Linux/bin/spades.py --pe1-1 lib1_forward_1.fastq \
+--pe1-2 lib1_reverse_1.fastq --pe1-1 lib1_forward_2.fastq --pe1-2 lib1_reverse_2.fastq -o spades_output
