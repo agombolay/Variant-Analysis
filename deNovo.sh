ae6dbaca496edbafc82cc27bc02739288806590f
Alli Gombolay <agombolay3@gmail.com>
2017-04-07 13:11:03 -0400

    Update deNovo.sh

--- deNovo.sh
+++ deNovo.sh
@@ -1,2 +1,5 @@
-/projects/home/agombolay3/data/bin/SPAdes-3.10.1-Linux/bin/spades.py --pe1-1 lib1_forward_1.fastq \
---pe1-2 lib1_reverse_1.fastq --pe1-1 lib1_forward_2.fastq --pe1-2 lib1_reverse_2.fastq -o spades_output
+#Assemble reference with de novo assembler
+/projects/home/agombolay3/data/bin/SPAdes-3.10.1-Linux/bin/spades.py \
+--pe1-1 YS486-1_S49_L001_R1_001.fastq.gz --pe1-2 YS486-1_S49_L001_R2_001.fastq.gz \
+--pe1-1 YS486-1_S49_L002_R1_001.fastq.gz --pe1-2 YS486-1_S49_L002_R2_001.fastq.gz \
+-o YS486-1-Spades-Output
