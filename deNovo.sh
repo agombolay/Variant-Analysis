c861d9f2abc7b5e64be07dfcf69e644bd59495c0
Alli Gombolay <agombolay3@gmail.com>
2017-04-07 21:26:21 -0400

    Update deNovo.sh

--- deNovo.sh
+++ deNovo.sh
@@ -5,3 +5,5 @@ path=/projects/home/agombolay3/data/repository/Variant-Calling-Project/Sequencin
 --pe1-1 $path/YS486-1_S49_L001_R1_001.fastq.gz --pe1-2 $path/YS486-1_S49_L001_R2_001.fastq.gz \
 --pe1-1 $path/YS486-1_S49_L002_R1_001.fastq.gz --pe1-2 $path/YS486-1_S49_L002_R2_001.fastq.gz \
 -o YS486-1-Spades-Output
+
+bowtie2-build scaffolds.fasta scaffolds
