42ec56fda95c61bef4122dfc8bd4b5f24448d2e6
Alli Gombolay <agombolay3@gmail.com>
2017-04-12 12:32:14 -0400

    Update call-variants.sh

--- call-variants.sh
+++ call-variants.sh
@@ -6,7 +6,7 @@
 #Usage statement
 function usage () {
         echo "Usage: call-variants.sh [-s] 'sample' [-h]
-	      -s Sample name of library; used to name output files
+	      -s Sample name of library; used to name output files"
 }
 
 #Command-line options
@@ -63,4 +63,4 @@ java -jar $bin/GenomeAnalysisTK.jar -I $sample-MarkDups.bam -ERC GVCF -o $sample
 #java -Xmx4g -jar /projects/home/agombolay3/data/bin/snpEff/snpEff.jar sacCer2 Variants1.vcf  > Variants1-Annotated.vcf 
 
 #Filter variants in VCF file by quality score
-#cat Variants1-Annotated.vcf | java -jar $bin/snpEff/SnpSift.jar filter ( QUAL >= 30 ) > Variants1-Filtered.vcf
+#cat Variants1-Annotated.vcf | java -jar $bin/snpEff/SnpSift.jar filter "( QUAL >= 30 )" > Variants1-Filtered.vcf
