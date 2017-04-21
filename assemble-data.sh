1b273cbe0ac66935b5b6cf79309f4600b6d172b8
Alli Gombolay <agombolay3@gmail.com>
2017-04-21 11:59:54 -0400

    Update assemble-data.sh

--- assemble-data.sh
+++ assemble-data.sh
@@ -41,58 +41,58 @@ bin=/projects/home/agombolay3/data/bin
 reference=/projects/home/agombolay3/data/repository/Variant-Calling-Project/Variant-Calling/sacCer2.fa
 
 #Remove old version of output
-#rm -f $sample-R1.fq.gz $sample-R2.fq.gz $sample-R1Paired.fq \
-#$sample-R2Paired.fq $sample.sam $sample.bam $sample.bam.bai
+rm -f $sample-R1.fq.gz $sample-R2.fq.gz $sample-R1Paired.fq \
+$sample-R2Paired.fq $sample.sam $sample.bam $sample.bam.bai
 
 #Concatenate FASTQ files from lanes 1 and 2
-#cat $inputForward1 $inputForward2 > $sample-R1.fq.gz
-#cat $inputReverse1 $inputReverse2 > $sample-R2.fq.gz
+cat $inputForward1 $inputForward2 > $sample-R1.fq.gz
+cat $inputReverse1 $inputReverse2 > $sample-R2.fq.gz
 
 #STEP 1
 #Trim FASTQ files based on quality and Illumina adapter content
-#java -jar $path/trimmomatic-0.36.jar PE -phred33 $sample-R1.fq.gz $sample-R2.fq.gz \
-#$sample-R1Paired.fq.gz $sample-R1Unpaired.fq.gz $sample-R2Paired.fq.gz $sample-R2Unpaired.fq.gz \
-#ILLUMINACLIP:$path/adapters/NexteraPE-PE.fa:2:30:10 LEADING:10 TRAILING:10 SLIDINGWINDOW:5:15 MINLEN:75
+java -jar $path/trimmomatic-0.36.jar PE -phred33 $sample-R1.fq.gz $sample-R2.fq.gz \
+$sample-R1Paired.fq.gz $sample-R1Unpaired.fq.gz $sample-R2Paired.fq.gz $sample-R2Unpaired.fq.gz \
+ILLUMINACLIP:$path/adapters/NexteraPE-PE.fa:2:30:10 LEADING:10 TRAILING:10 SLIDINGWINDOW:5:15 MINLEN:75
 
 #Unzip files
-#gunzip $sample-R1Paired.fq.gz $sample-R2Paired.fq.gz
+gunzip $sample-R1Paired.fq.gz $sample-R2Paired.fq.gz
 
 #STEP 2
 #Align trimmed forward and reverse reads to reference genome of interest
-#bowtie2 -x sacCer2 -1 $sample-R1Paired.fq -2 $sample-R2Paired.fq -S $sample.sam
+bowtie2 -x sacCer2 -1 $sample-R1Paired.fq -2 $sample-R2Paired.fq -S $sample.sam
 #bowtie2 -x scaffolds -1 $sample-R1Paired.fq -2 $sample-R2Paired.fq -S $sample.sam
 
 #Convert SAM file to BAM file format and sort BAM file
-#samtools view -b -S $sample.sam | samtools sort -o $sample.bam -
+samtools view -b -S $sample.sam | samtools sort -o $sample.bam -
 
 #Create index file
-#samtools index $sample.bam
+samtools index $sample.bam
 
 #Move reads to subfolder
-#mkdir Reads; mv $sample-R1.fq.gz Reads; mv $sample-R2.fq.gz Reads; mv $sample-R1Paired.fq Reads
-#mv $sample-R2Paired.fq Reads; mv $sample-R1Unpaired.fq.gz Reads; mv $sample-R2Unpaired.fq.gz Reads
+mkdir Reads; mv $sample-R1.fq.gz Reads; mv $sample-R2.fq.gz Reads; mv $sample-R1Paired.fq Reads
+mv $sample-R2Paired.fq Reads; mv $sample-R1Unpaired.fq.gz Reads; mv $sample-R2Unpaired.fq.gz Reads
 
 #Create FASTA index file
-samtools faidx sacCer2.fa
+#samtools faidx sacCer2.fa
 
 #Create FASTA dictionary file
-java -jar $bin/picard.jar CreateSequenceDictionary R=$reference O=sacCer2.dict
+#java -jar $bin/picard.jar CreateSequenceDictionary R=$reference O=sacCer2.dict
 
 #STEP 3
 #Add read groups to alignment file
-java -jar $bin/picard.jar AddOrReplaceReadGroups I=$sample.bam O=$sample-AddReadGroups.bam \
-RGLB=$sample RGPL=Illumina RGPU=HiSeq RGSM=$sample #LB = library, PL = platform, SM = sample
+#java -jar $bin/picard.jar AddOrReplaceReadGroups I=$sample.bam O=$sample-AddReadGroups.bam \
+#RGLB=$sample RGPL=Illumina RGPU=HiSeq RGSM=$sample #LB = library, PL = platform, SM = sample
 
 #STEP 4
 #Mark duplicates (account for PCR duplicates)
-java -jar $bin/picard.jar MarkDuplicates I=$sample-AddReadGroups.bam O=$sample-MarkDups.bam M=$sample.metrics.txt
+#java -jar $bin/picard.jar MarkDuplicates I=$sample-AddReadGroups.bam O=$sample-MarkDups.bam M=$sample.metrics.txt
 
 #Create index file
-samtools index $sample-MarkDups.bam
+#samtools index $sample-MarkDups.bam
 
 #STEP 5
 #Call variants
-java -jar $bin/GenomeAnalysisTK.jar -I $sample-MarkDups.bam -ERC GVCF -o $sample.g.vcf -T HaplotypeCaller -R $reference
+#java -jar $bin/GenomeAnalysisTK.jar -I $sample-MarkDups.bam -ERC GVCF -o $sample.g.vcf -T HaplotypeCaller -R $reference
 
 #STEP 6
 #Joint genotyping
