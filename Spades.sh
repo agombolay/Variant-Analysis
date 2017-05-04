d5a4151ce81f8f278dce9d1d9e40c1fa059c0234
Alli Gombolay <agombolay3@gmail.com>
2017-05-04 12:48:57 -0400

    Update Spades.sh

--- Spades.sh
+++ Spades.sh
@@ -14,5 +14,8 @@ zcat $path/YS486-1_S49_L001_R2_001.fastq.gz $path/YS486-1_S49_L002_R2_001.fastq.
 zcat $path/YS486-2_S58_L001_R1_001.fastq.gz $path/YS486-2_S58_L002_R1_001.fastq.gz > $path/YS486-2_R1.fastq.gz
 zcat $path/YS486-2_S58_L001_R2_001.fastq.gz $path/YS486-2_S58_L002_R2_001.fastq.gz > $path/YS486-2_R2.fastq.gz
 
+#Unzip files
+gunzip $path/YS486-1_R1.fastq.gz $path/YS486-1_R2.fastq.gz $path/YS486-2_R1.fastq.gz $path/YS486-2_R2.fastq.gz
+
 #Run Spades
 spades.py --pe<1>-1 $path/YS486-1_R1.fastq.gz --pe<1>-2 $path/YS486-1_R2.fastq.gz --pe<2>-1 $path/YS486-2_R1.fastq.gz --pe<2>-2 $path/YS486-2_R2.fastq.gz --careful -o $output
