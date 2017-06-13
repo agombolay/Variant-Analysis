8a9b5a41b7171e156c7e2d236b56f9c163f38022
Alli Gombolay <agombolay3@gmail.com>
2017-06-13 12:00:23 -0400

    Create Alignment.sh

--- Alignment.sh
+++ Alignment.sh
@@ -1,29 +1,32 @@
 #!/usr/bin/env bash
 
 #Author: Alli Gombolay
-#This script identifies variants among cases and controls
+#This script aligns controls to reference genome
 
 #Usage statement
 function usage () {
-        echo "Usage: call-variants.sh [-s] 'sample' [-abcd] 'FASTQ.GZ' [-p] '/path/to/Trimmomatic/' [-h]
-	      -a Path to input R1 FASTQ.GZ files, lane 1
-	      -b Path to input R1 FASTQ.GZ files, lane 2
-	      -c Path to input R2 FASTQ.GZ files, lane 1
-	      -d Path to input R2 FASTQ.GZ files, lane 2
-	      -s Sample name of library; used to name output files
-	      -p '/projects/home/agombolay3/data/bin/Trimmomatic-0.36'"
+        echo "Usage: Alignment.sh [-s] 'Sample(s)' [-abcd] 'FASTQ.GZ' [-p] 'Path' [-i] 'Index' [-d] 'Directory' [-h]
+		-s Sample name(s) (e.g., FS1, FS2, FS3)
+		-a Input Read 1, lane 1 FASTQ.GZ filename
+	      	-b Input Read 1, lane 2 FASTQ.GZ filename
+	      	-c Input Read 2, lane 1 FASTQ.GZ filename
+	      	-d Input Read 2, lane 2 FASTQ.GZ filename
+	      	-p Path (e.g., /projects/home/agombolay3/data/bin/Trimmomatic-0.36)
+	      	-i Basename of Bowtie2 index (e.g., sacCer2, pombe, ecoli, mm9, or hg38)
+		-d Local user directory (e.g., /projects/home/agombolay3/data/repository)"
 }
 
 #Command-line options
-while getopts "a:b:c:d:s:p:h" opt;
-do
+while getopts "s:w:x:y:z:p:i:d:h" opt; do
     case $opt in
-	a ) inputForward1=$OPTARG ;;
-	b ) inputForward2=$OPTARG ;;
-        c ) inputReverse1=$OPTARG ;;
-	d ) inputReverse2=$OPTARG ;;
 	s ) sample=$OPTARG ;;
+	w ) forward1=$OPTARG ;;
+	x ) forward2=$OPTARG ;;
+        y ) reverse1=$OPTARG ;;
+	z ) reverse2=$OPTARG ;;
         p ) path=$OPTARG ;;
+	i ) index=$OPTARG ;;
+	d ) directory=$OPTARG ;;
         #Print usage statement
         h ) usage ;;
     esac
@@ -34,40 +37,31 @@ if [ "$1" == "-h" ]; then
         exit
 fi
 
-#Path to bin folder
-bin=/projects/home/agombolay3/data/bin
+#Input files
+input1=$directory/Variant-Calling-Project/Sequencing/$forward1
+input2=$directory/Variant-Calling-Project/Sequencing/$forward2
+input3=$directory/Variant-Calling-Project/Sequencing/$reverse1
+input4=$directory/Variant-Calling-Project/Sequencing/$reverse2
 
 #Path to reference file
-reference=/projects/home/agombolay3/data/repository/Variant-Calling-Project/Variant-Calling/sacCer2.fa
-
-#Remove old version of output
-rm -f $sample-R1.fq.gz $sample-R2.fq.gz $sample-R1Paired.fq \
-$sample-R2Paired.fq $sample.sam $sample.bam $sample.bam.bai
+reference=$directory/Variant-Calling-Project/References/$reference.fa
 
 #Concatenate FASTQ files from lanes 1 and 2
-cat $inputForward1 $inputForward2 > $sample-R1.fq.gz
-cat $inputReverse1 $inputReverse2 > $sample-R2.fq.gz
+cat $input1 $input2 > $sample-R1.fq.gz; cat $input3 $input4 > $sample-R2.fq.gz
 
+#############################################################################################################################
 #STEP 1
 #Trim FASTQ files based on quality and Illumina adapter content
 java -jar $path/trimmomatic-0.36.jar PE -phred33 $sample-R1.fq.gz $sample-R2.fq.gz \
 $sample-R1Paired.fq.gz $sample-R1Unpaired.fq.gz $sample-R2Paired.fq.gz $sample-R2Unpaired.fq.gz \
-ILLUMINACLIP:$path/adapters/NexteraPE-PE.fa:2:30:10 LEADING:10 TRAILING:10 SLIDINGWINDOW:5:15 MINLEN:75
+ILLUMINACLIP:$path/adapters/NexteraPE-PE.fa:2:30:10 LEADING:10 TRAILING:10 SLIDINGWINDOW:4:15 MINLEN:75
 
 #Unzip files
 gunzip $sample-R1Paired.fq.gz $sample-R2Paired.fq.gz
 
-#STEP 2
-#Align trimmed forward and reverse reads to reference genome of interest
-bowtie2 -x sacCer2 -1 $sample-R1Paired.fq -2 $sample-R2Paired.fq -S $sample.sam
-#bowtie2 -x scaffolds -1 $sample-R1Paired.fq -2 $sample-R2Paired.fq -S $sample.sam
+#############################################################################################################################
+#STEP 2: Align pairs of reads to reference genome and save Bowtie2 log file
+bowtie2 -x $index -1 $sample-R1Paired.fq -2 $sample-R2Paired.fq 2> $statistics > $sample.sam
 
-#Convert SAM file to BAM file format and sort BAM file
-samtools view -b -S $sample.sam | samtools sort -o $sample.bam -
-
-#Create index file
-samtools index $sample.bam
-
-#Move reads to subfolder
-mkdir Reads; mv $sample-R1.fq.gz Reads; mv $sample-R2.fq.gz Reads; mv $sample-R1Paired.fq Reads
-mv $sample-R2Paired.fq Reads; mv $sample-R1Unpaired.fq.gz Reads; mv $sample-R2Unpaired.fq.gz Reads
+#STEP 3: Extract mapped reads, convert SAM file to BAM, and sort/index BAM file
+samtools view -bSf2 $sample.sam | samtools sort - -o $sample.bam; samtools index $sample.bam
