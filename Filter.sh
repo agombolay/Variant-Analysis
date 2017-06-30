fe88b8a81aab02ad472311fbe8d96ff5384ab20c
Alli Gombolay <agombolay3@gmail.com>
2017-06-30 12:15:30 -0400

    Update Filter.sh

--- Filter.sh
+++ Filter.sh
@@ -1,11 +1,8 @@
-#Filter variants in VCF file by quality score with SnpSift
-#cat Variants.vcf | java -jar $snpSift filter "((QUAL >= 30) && (DP >= 25))" > Variants-Filtered.vcf
+#Filter variants in VCF file by quality score
+cat Variants.vcf | java -jar $snpSift filter "((QUAL >= 30) && (DP >= 25))" > Variants-Filtered.vcf
 
-#java -jar $snpSift extractFields Variants-Filtered.vcf "CHROM" "POS" "REF" "ALT" "GEN[*].GT" > out.vcf
+java -jar $snpSift extractFields Variants-Filtered.vcf "CHROM" "POS" "REF" "ALT" "GEN[*].GT" > out.vcf
 
-#awk -F'\t' '$6!=$7 {print $0}' out.vcf
+awk -F'\t' '$6!=$7 {print $0}' out.vcf
 
 awk -F'\t' '($6!= "./.") {print $0}' out2.vcf
-
-#Create tab-delimited file of variants
-#java -jar $gatk -R $reference -T VariantsToTable -V Variants-Filtered.vcf -F CHROM -F POS -F QUAL -o Variants.table
