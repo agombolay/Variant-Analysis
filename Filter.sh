13e2907af196093fc5451a2b0fccf055e969b333
Alli Gombolay <agombolay3@gmail.com>
2017-06-30 14:10:15 -0400

    Update Filter.sh

--- Filter.sh
+++ Filter.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+#Author: Alli Gombolay
+#This script filter variants in VCF file
+
 snpSift=/projects/home/agombolay3/data/bin/snpEff/SnpSift.jar
 
 #Filter variants in VCF file by quality score
@@ -11,3 +16,6 @@ awk -F'\t' '$6!=$7 {print $0}' Variants-Filtered2.vcf > Variants-Filtered3.vcf
 
 #Keep variants for which the controls do not have a genotype
 awk -F'\t' '($6== "./.") {print $0}' Variants-Filtered3.vcf > Variants-Filtered4.vcf
+
+#Remove temporary files
+rm -f Variants-Filtered1.vcf Variants-Filtered2.vcf Variants-Filtered3.vcf
