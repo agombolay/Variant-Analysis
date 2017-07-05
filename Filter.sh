a8e4974d9146ddd4aba96b49f15c709e281f7534
Alli Gombolay <agombolay3@gmail.com>
2017-07-04 23:46:16 -0400

    Update Filter.sh

--- Filter.sh
+++ Filter.sh
@@ -3,16 +3,8 @@
 #Author: Alli Gombolay
 #This script filter variants in VCF file
 
-snpSift=/projects/home/agombolay3/data/bin/snpEff/SnpSift.jar
+#Filter by quality and depth, extract fields, and remove variants where GT of control and cases are same
+cat Variants.vcf | java -jar /projects/home/agombolay3/data/bin/snpEff/SnpSift.jar filter "((QUAL >= 30) && (DP >= 25))" | \
+java -jar /projects/home/agombolay3/data/bin/snpEff/SnpSift.jar extractFields - "CHROM" "POS" "REF" "ALT" "GEN[*].GT" | \
+awk -F'\t' '$12!=$5 || $12!=$6 || $12!=$7 || $12!=$8 || $12!=$9 || $12!=$10 || $12!=$11 {print $0}' - > Variants.tab
 
-#Filter variants in VCF file by quality score
-cat Variants.vcf | java -jar $snpSift filter "((QUAL >= 30) && (DP >= 25))" > Variants-Filtered1.vcf
-
-#Extract chrom, position, ref, alt, and GT from VCF file
-java -jar $snpSift extractFields Variants-Filtered1.vcf "CHROM" "POS" "REF" "ALT" "GEN[*].GT" > Variants-Filtered2.vcf
-
-#Remove variants where GT of controls and cases are same
-awk -F'\t' '$5!=$6 && $5!=$7 && $5!=$8 && $5!=$9 && $5!=$10 && $5!=$11 && $5!=$12 {print $0}' Variants-Filtered2.vcf > Variants-Filtered3.vcf
-
-#Remove temporary files
-rm -f Variants-Filtered1.vcf Variants-Filtered2.vcf Variants-Filtered3.vcf
