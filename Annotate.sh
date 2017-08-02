91f5eb0ca65029d1712cc8d02f991d4132665148
Alli Gombolay <agombolay3@gmail.com>
2017-08-02 13:11:40 -0400

    Update Annotate.sh

--- Annotate.sh
+++ Annotate.sh
@@ -1,1 +1,6 @@
-java -Xmx4g -jar snpEff.jar GRCh37.75 examples/test.chr22.vcf > test.chr22.ann.vcf
+#!/usr/bin/env bash
+
+#Author: Alli Gombolay
+#This script annotates variants in VCF file
+
+java -Xmx4g -jar snpEff.jar Saccharomyces_cerevisiae examples/test.chr22.vcf > test.chr22.ann.vcf
