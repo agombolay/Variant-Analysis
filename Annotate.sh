a935e28f185739bd9edfc5e008a887a2db68c6ab
Alli Gombolay <agombolay3@gmail.com>
2017-08-03 15:31:04 -0400

    Delete Annotate.sh

--- Annotate.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-#Author: Alli Gombolay
-#This script annotates variants in VCF file
-
-java -Xmx4g -jar snpEff.jar Saccharomyces_cerevisiae Variants.vcf > Variants-Annotated.vcf
