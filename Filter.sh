6179e9463ffe1457a796928bb26d8d0d54fdd80c
Alli Gombolay <agombolay3@gmail.com>
2017-08-03 15:38:13 -0400

    Update Filter.sh

--- Filter.sh
+++ Filter.sh
@@ -11,6 +11,6 @@ java -Xmx4g -jar $SnpEff Saccharomyces_cerevisiae Variants.vcf > Variants-Annota
 #Filter by quality and depth, extract fields, and remove variants where GT of control and cases are same
 cat Variants-Annotated.vcf | java -jar $SnpSift filter "((QUAL >= 30) && (DP >= 25))" | \
 
-java -jar $SnpSift extractFields - "CHROM" "POS" "REF" "ALT" "GEN[*].GT" | \
+java -jar $SnpSift filter "( EFF[*].EFFECT !~  'SYNONYMOUS_CODING' )" \ java -jar $SnpSift extractFields - "CHROM" "POS" "REF" "ALT" "GEN[*].GT" | \
 
 awk -F'\t' '$12!=$5 || $12!=$6 || $12!=$7 || $12!=$8 || $12!=$9 || $12!=$10 || $12!=$11 {print $0}' - > Variants-Filtered.tab
