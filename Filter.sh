e2a2f5ca703f2654aa9fd212c443ed0c19228d75
Alli Gombolay <agombolay3@gmail.com>
2017-08-03 15:33:21 -0400

    Update Filter.sh

--- Filter.sh
+++ Filter.sh
@@ -3,9 +3,14 @@
 #Author: Alli Gombolay
 #This script annotates and filters variants in VCF file
 
-java -Xmx4g -jar snpEff.jar Saccharomyces_cerevisiae Variants.vcf > Variants-Annotated.vcf
+SnpEff=/projects/home/agombolay3/data/bin/snpEff/SnpEff.jar
+SnpSift=/projects/home/agombolay3/data/bin/snpEff/SnpSift.jar
+
+java -Xmx4g -jar $SnpEff Saccharomyces_cerevisiae Variants.vcf > Variants-Annotated.vcf
 
 #Filter by quality and depth, extract fields, and remove variants where GT of control and cases are same
-cat Variants.vcf | java -jar /projects/home/agombolay3/data/bin/snpEff/SnpSift.jar filter "((QUAL >= 30) && (DP >= 25))" | \
-java -jar /projects/home/agombolay3/data/bin/snpEff/SnpSift.jar extractFields - "CHROM" "POS" "REF" "ALT" "GEN[*].GT" | \
-awk -F'\t' '$12!=$5 || $12!=$6 || $12!=$7 || $12!=$8 || $12!=$9 || $12!=$10 || $12!=$11 {print $0}' - > Variants.tab
+cat Variants-Annotated.vcf | java -jar $SnpSift filter "((QUAL >= 30) && (DP >= 25))" | \
+
+java -jar $SnpSift extractFields - "CHROM" "POS" "REF" "ALT" "GEN[*].GT" | \
+
+awk -F'\t' '$12!=$5 || $12!=$6 || $12!=$7 || $12!=$8 || $12!=$9 || $12!=$10 || $12!=$11 {print $0}' - > Variants-Filtered.tab
