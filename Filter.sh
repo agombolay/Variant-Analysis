ee6daff87d0c31033e14d6ab8a83b11160006072
Alli Gombolay <agombolay3@gmail.com>
2017-08-03 16:20:00 -0400

    Update Filter.sh

--- Filter.sh
+++ Filter.sh
@@ -9,7 +9,8 @@ SnpSift=/projects/home/agombolay3/data/bin/snpEff/SnpSift.jar
 java -Xmx4g -jar $SnpEff Saccharomyces_cerevisiae Variants.vcf > Variants-Annotated.vcf
 
 #Filter by quality and depth and remove variants where GT of control and cases are same
-cat Variants-Annotated.vcf | java -jar $SnpSift filter "((QUAL >= 30) && (DP >= 25))" | \
-extractFields "CHROM" "POS" "REF" "ALT" "GEN[*].GT" "ANN[*].EFFECT" > Variants.tab
+cat Variants-Annotated.vcf | java -jar $SnpSift filter "((QUAL >= 30) && (DP >= 25))" > Variants.tab
+
+cat Variants.tab | java -jar $SnpSift extractFields "CHROM" "POS" "REF" "ALT" "GEN[*].GT" "ANN[*].EFFECT" > Variants2.tab
 
 #awk -F'\t' '$12!=$5 || $12!=$6 || $12!=$7 || $12!=$8 || $12!=$9 || $12!=$10 || $12!=$11 {print $0}' > Variants.tab
