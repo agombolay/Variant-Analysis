4ba139b0f0e97f0196e2546791db75db027515db
Alli Gombolay <agombolay3@gmail.com>
2017-08-03 15:30:36 -0400

    Update Filter.sh

--- Filter.sh
+++ Filter.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 #Author: Alli Gombolay
-#This script filter variants in VCF file
+#This script annotates and filters variants in VCF file
+
+java -Xmx4g -jar snpEff.jar Saccharomyces_cerevisiae Variants.vcf > Variants-Annotated.vcf
 
 #Filter by quality and depth, extract fields, and remove variants where GT of control and cases are same
 cat Variants.vcf | java -jar /projects/home/agombolay3/data/bin/snpEff/SnpSift.jar filter "((QUAL >= 30) && (DP >= 25))" | \
 java -jar /projects/home/agombolay3/data/bin/snpEff/SnpSift.jar extractFields - "CHROM" "POS" "REF" "ALT" "GEN[*].GT" | \
 awk -F'\t' '$12!=$5 || $12!=$6 || $12!=$7 || $12!=$8 || $12!=$9 || $12!=$10 || $12!=$11 {print $0}' - > Variants.tab
-
