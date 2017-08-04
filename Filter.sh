f97e43aaf4efdda9f649bb2ca8d60ccfd46f8ec8
Alli Gombolay <agombolay3@gmail.com>
2017-08-04 12:07:22 -0400

    Update Filter.sh

--- Filter.sh
+++ Filter.sh
@@ -15,10 +15,11 @@ cat Variants-Annotated.vcf | java -jar $SnpSift filter "((QUAL >= 30) && (DP >= 
 java -jar $SnpSift extractFields Variants1.tab "CHROM" "POS" "REF" "ALT" "GEN[*].GT" "ANN[*].EFFECT" > Variants2.tab
 
 #Remove variants where genotype of control = cases
-awk -F'\t' '$12!=$5 || $12!=$6 || $12!=$7 || $12!=$8 || $12!=$9 || $12!=$10 || $12!=$11 {print $0}' Variants2.tab > Variants3.tab
+awk -F'\t' '$12!=$5||$12!=$6||$12!=$7||$12!=$8||$12!=$9||$12!=$10||$12!=$11 {print $0}' Variants2.tab > Variants3.tab
 
 #Remove variants that are synonymous for all samples
-awk -F'\t' '$13!='synonymous_variant' || $14!='synonymous_variant' || $15!='synonymous_variant' || $16!='synonymous_variant' || $17!='synonymous_variant' {print $0}' Variants3.tab > Variants.tab
+awk -F'\t' '$13!='synonymous_variant'||$14!='synonymous_variant'||$15!='synonymous_variant'||$16!='synonymous_variant'\
+||$17!='synonymous_variant' {print $0}' Variants3.tab > Variants.tab
 
 #Remove temporary files
 rm -f Variants1.tab Variants2.tab Variants4.tab
